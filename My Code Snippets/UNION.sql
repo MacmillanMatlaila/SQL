14b2f8fc5fbb1206d381f9879687fd86d984394f
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2025-06-03 12:56:47 +0200

    USE CASE - COMBINING DATA

--- My Code Snippets/UNION.sql
+++ My Code Snippets/UNION.sql
@@ -3,13 +3,39 @@
 /*Order of columns in each query must be the same*/
 
 SELECT
-FirstName, /*Naming of this first query is controlled here, aliases of output columns can be done here*/
-LastName
+FirstName AS 'LEINA LA MATHOMO', /*Naming of this first query is controlled here, aliases of output columns can be done here*/
+LastName AS 'LEINA LA MAFELELO'
 FROM Sales.Customers
 
 UNION
 
 SELECT
+FirstName,
+LastName
+FROM Sales.Employees
+
+--COMBINE DATA FROM EMPLOYEES AND CUSTOMERS INTO ONE
+
+SELECT 
+FirstName,
+LastName
+FROM Sales.Customers
+
+UNION --removes duplicates
+
+SELECT 
+FirstName,
+LastName
+FROM Sales.Employees
+
+SELECT 
+FirstName,
+LastName
+FROM Sales.Customers
+
+UNION ALL --returns duplicates
+
+SELECT 
 FirstName,
 LastName
 FROM Sales.Employees
